a253d4f93e78a45364bc126f4e9edf58ea25878d
remcostroop <D284662@edu.rocwb.nl>
2021-11-08 12:41:42 +0100

    Version 1.0 Finished
    Version 1.0 has been released.

---
+++ FibonacciNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Verschillende int variabelen aanmaken:
+            int number, firstNumber = 0, secondNumber = 1, NextNumber, i;
+            // Vraag aan gebruiker hoeveel getallen er getoont moeten worden:
+            Console.Write("Vul hier het aantal nummers in dat u wilt tonen : ");
+            number = Int32.Parse(Console.ReadLine());
+            // For loop + If en Else statement
+            for (i = 0; i < number; i++)
+            {
+                // Als i kleiner of gelijk is aan i ga naar de writeline:
+                if (i <= 1)
+                    NextNumber = i;
+                // Anders doe berekening voor volgende getal en toon gebruiker::
+                else
+                {
+                    NextNumber = firstNumber + secondNumber;
+                    firstNumber = secondNumber;
+                    secondNumber = NextNumber;
+                }
+                // Writeline om de gebruiker te laten tonen:
+                Console.Write(NextNumber + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
